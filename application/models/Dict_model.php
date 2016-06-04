47bbf8f57e111740275be67e104875f48defcf9b
wsdjeg <wsdjeg@outlook.com>
2016-06-04 20:28:53 +0800

    improve search logic

--- application/models/Dict_model.php
+++ application/models/Dict_model.php
@@ -8,6 +8,8 @@ class Dict_model extends CI_Model
       parent::__construct();
     }
 
+
+
     function getStudyUnits($study_units){
 
         $sql = "";
@@ -56,7 +58,9 @@ class Dict_model extends CI_Model
 
     function searchUnits($stringIn) {
         // $query = str_replace(" ", "%", $query);
-        $parts = preg_split("/[\s,]+/", $stringIn);
+        $parts = preg_split("/ /", $stringIn);
+        echo $stringIn;
+        print_r($parts);
 
         $enParts = [];
         $cnParts = [];
@@ -65,7 +69,6 @@ class Dict_model extends CI_Model
             // if( isCN($v) ) {
             // if(!preg_match("[^\x80-\xff]","$v")) {
             if (preg_match("/[\x7f-\xff]/", $v)) {
-
                 array_push($cnParts, $v);
             } else {
                 array_push($enParts, $v);
@@ -75,6 +78,8 @@ class Dict_model extends CI_Model
         // print_r($enParts);
         // print_r($cnParts);
 
+
+
         $enGlue = "%' and i.Keywords like '%";
         $enMatchAllKeywords = join($enGlue, $enParts);
 
@@ -82,25 +87,117 @@ class Dict_model extends CI_Model
         $cnMatchAllKeywords = join($cnGlue, $cnParts);
 
 
-        $sql = "SELECT *
-                FROM grammarDict d 
-                Inner Join grammarIndex i
-                    On i.Keywords like '%$enMatchAllKeywords%'
-                Inner Join grammarKeyPoints gkp 
-                    on gkp.Grammar_Point_ like '%$cnMatchAllKeywords%'
-                group by d.no
+        // dynamic filter
 
-                having LOCATE(d.no, i.Units_index) > 0
-                    and LOCATE(d.no, gkp.related_units) > 0
+        // $enSnippet = '';
+        // $enHaving = '';
+        // $cnSnippet = '';
+        // $cnHaving = '';
 
-                ";
+        // $havingParts = [];
+        // $havingSnippet = '';
+
+
+        // if ( count($enParts) > 0 ) {
+        //     $enSnippet = "Inner Join grammarIndex i
+        //                 On i.Keywords like '%$enMatchAllKeywords%'";
+
+        //     $enHaving = "LOCATE(d.no, i.Units_index) > 0";
+        //     array_push($havingParts, $enHaving);
+        // }
+
+        // if ( count($cnParts) > 0 ) {
+        //     $cnSnippet = "Inner Join grammarKeyPoints gkp
+        //                 on gkp.Grammar_Point_ like '%$cnMatchAllKeywords%'";
+
+        //     $cnHaving = "LOCATE(d.no, gkp.related_units) > 0";
+        //     array_push($havingParts, $cnHaving);
+        // }
+
+        // if ( count($cnParts) + count($enParts) > 0 ) {
+        //     $havingSnippet = "having ".join($havingParts, ' and ');
+        // }
+
+        // $sql = "SELECT *
+        //         FROM grammarDict d 
+        //             $enSnippet
+        //             $cnSnippet
+
+        //         group by d.no
+
+        //         $havingSnippet
+
+        //         ";
 
 
         // echo $sql;
 
+        // echo count(array_filter($enParts));
+        // echo count(array_filter($cnParts));
+
+        $spotEnUnits = [];
+        $spotCnUnits = [];
+
+        if ( count(array_filter($enParts)) > 0 ) {
+            $sqlEn = "select i.Units_index from grammarIndex i
+                where i.keywords like '%$enMatchAllKeywords%'";
+echo $sqlEn;
+            $enResults = $this->db->query($sqlEn)->result();
+
+            $spotEnUnits = $this->uniqueResult($enResults, 'Units_index');
+
+            // echo "en==";
+            print_r($enResults);
+        }
+
+        if ( count(array_filter($cnParts)) > 0 ) {
+            $sqlCn = "select gkp.Related_Units from grammarKeyPoints gkp 
+                where gkp.Grammar_Point_ like '%$cnMatchAllKeywords%'";
+
+            $cnResults = $this->db->query($sqlCn)->result();
+
+            $spotCnUnits = $this->uniqueResult($cnResults, 'Related_Units');
+            // echo "cn===";
+
+        }
+
+
+
+
+        $spottedUnits = array_merge($spotEnUnits, $spotCnUnits);
+        $spotUnitsString = join(',', array_unique($spottedUnits));
+
+
+        $spotUnitsString = str_replace(",,", ",", $spotUnitsString);
+
+        $sql = "select * from grammarDict where No in ($spotUnitsString)";
+        echo $sql;
+
         $results = $this->db->query($sql)->result();
+
+
+
         return $results;
+
     }
+
+
+
+    function uniqueResult($inResults, $column) {
+        $displayUnits = [];
+
+        foreach ($inResults as $result) {
+            $units = preg_split('/[;,]+/', $result->$column);
+            if( count($units) > 0) {
+                $displayUnits = array_merge( $displayUnits, $units );
+            }
+        }
+
+        $displayUnits = array_unique($displayUnits);
+
+        return $displayUnits;
+    }
+
 
 
 
